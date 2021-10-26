b116fc9bd7d1f802ecf52780d05b2c96aea1a5dc
Arpan Samariya <arpansamria16@gmail.com>
2021-10-26 04:56:49 +0000

    Sync LeetCode submission - Invert Binary Tree (cpp)

--- problems/invert_binary_tree/solution.cpp
+++ problems/invert_binary_tree/solution.cpp
@@ -11,46 +11,24 @@
  */
 class Solution {
 public:
-    
     TreeNode* invertTree(TreeNode* root) {
-// Recursive
-//         if(root == NULL)
-//             return NULL;
-//         if(root->left == NULL and root->right == NULL){
-//             return root;
-//         }
-        
-//         TreeNode *left , *right;
-//         left = invertTree(root->left);
-//         right = invertTree(root->right);
-//         root->left = right;
-//         root->right = left;
-//         return root;
-
-//Iterative
-        queue<TreeNode*> q;
-        q.push(root);
-        if(root == NULL)
-            return NULL;
-        while(!q.empty()){
-            TreeNode *curr, *temp, *left, *right;
-            curr = q.front();
-            q.pop();
-            // if(curr != NULL){
-            left = curr->left;
-            right = curr->right;
-            temp = left;
-            curr->left = right;
-            curr->right = temp;
-            if(curr->left != NULL){
-                q.push(curr->left);
-            }
-            if(curr->right != NULL){
-                q.push(curr->right);
-            }
-            // }
-            
-        }
+        invert(root);
         return root;
+    }
+    
+    void invert (TreeNode* root) {
+        if(root == NULL)
+            return;
+        
+        // if( root->left == NULL and root->right == NULL){
+        //     return;
+        // }
+        
+        invert(root->left);
+        invert(root->right);
+        
+        TreeNode* tmp = root->left;
+        root->left = root->right;
+        root->right = tmp;
     }
 };
