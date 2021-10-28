468ec7cd0dc551e97b33d9c79daf018c76ffe752
Arpan Samariya <arpansamria16@gmail.com>
2021-10-28 10:04:27 +0000

    Sync LeetCode submission - Two Sum (cpp)

--- problems/two_sum/solution.cpp
+++ problems/two_sum/solution.cpp
@@ -1,18 +1,52 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        map<int, int> mp;
-        vector<int> ans;
-        for(int i = 0; i < nums.size()-1; i++){
-            for(int j = i+1; j < nums.size(); j++){
-                if(nums[i]+nums[j] == target)
-                {
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    return ans;
+        
+        int left = 0;
+        vector<int> vc;
+        map<int,int> mp;
+
+        for(int i = 0 ; i < nums.size(); i++) {
+           mp[nums[i]] = i;
+        }
+        
+        for(int i = 0 ; i < nums.size(); i++) {
+            auto itr = mp.find(target - nums[i]);
+           if( itr != mp.end() and i != itr->second ){
+               vc.push_back(i);
+               vc.push_back(mp[target - nums[i] ]);
+               return vc;
+           }
+               
+        }
+        
+        
+         return vc;
+    }
+        
+};
+
+
+
+/*
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        int left = 0;
+        vector<int> vc;
+
+        for(int i = 0 ; i < nums.size() -1; i++) {
+            for(int j = i+1; j < nums.size(); j++) {
+                if(nums[i] + nums[j] == target){
+                    vc.push_back(i);
+                    vc.push_back(j);
+                    return vc;
                 }
             }
         }
-     return ans;   
+         return vc;
     }
+        
 };
+*/
