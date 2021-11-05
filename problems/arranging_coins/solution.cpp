7db14a3fe81cb55e967d396aff220467bd377df3
Arpan Samariya <arpansamria16@gmail.com>
2021-11-05 05:23:46 +0000

    Sync LeetCode submission - Arranging Coins (cpp)

--- problems/arranging_coins/solution.cpp
+++ problems/arranging_coins/solution.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-       unsigned long long int k, curr;
-     // return (-1+sqrt(1+4*2*k))/2;
-        // return n-((m*(m+1))/2);
-       // return m;
-        int left = 0, right = n;
-    while(left <= right){
-        k = left + (right-left)/2;
-        curr = k * (k+1) / 2;
-        if(curr == n){
-            return k;
+        unsigned long i = 0;
+        unsigned long t = n;
+        while(
+            i*(i+1) < 2*t
+        ){
+            i++;
         }
-        else if( curr < n){
-            left = k + 1;
-        }
-        else{
-            right = k -1;
-        }
-    }
-        return right;
+        if(i*(i+1) == 2*t)
+              return i;
+        return i-1;
     }
 };
