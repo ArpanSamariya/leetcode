542050cd8e9331cf8e40e9993e3576de0782a2d4
Arpan Samariya <arpansamria16@gmail.com>
2021-12-01 09:44:19 +0000

    Sync LeetCode submission - House Robber (cpp)

--- problems/house_robber/solution.cpp
+++ problems/house_robber/solution.cpp
@@ -1,5 +1,29 @@
 class Solution {
 public:
+    map<int,int> mp;
+    vector<int> arr;
+    int rob(vector<int>& nums) {
+        arr = nums;
+        return dp(nums.size()-1);
+    }
+    
+    int dp(int i){
+        
+        if(i==0) return arr[0];
+        if(i ==1) return max(arr[0], arr[1]);
+        
+        if(mp.find(i) == mp.end() ){
+            mp[i] = max(dp(i-1), dp(i-2) + arr[i] );
+        }
+        
+        return mp[i];
+    }
+};
+
+
+/*
+class Solution {
+public:
     int cnt = 0;
     map<int,int> mp;
     vector<int> arr;
@@ -20,3 +44,4 @@ public:
         return mp[n];
     }
 };
+*/
