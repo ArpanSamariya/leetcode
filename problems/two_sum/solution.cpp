4c74d1e24a2ebd141e91427caff2d992e7fb1d01
Arpan Samariya <arpansamria16@gmail.com>
2021-12-20 10:12:04 +0000

    Sync LeetCode submission - Two Sum (cpp)

--- problems/two_sum/solution.cpp
+++ problems/two_sum/solution.cpp
@@ -1,52 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
-        int left = 0;
-        vector<int> vc;
         map<int,int> mp;
-
-        for(int i = 0 ; i < nums.size(); i++) {
-           mp[nums[i]] = i;
-        }
-        
-        for(int i = 0 ; i < nums.size(); i++) {
-            auto itr = mp.find(target - nums[i]);
-           if( itr != mp.end() and i != itr->second ){
-               vc.push_back(i);
-               vc.push_back(mp[target - nums[i] ]);
-               return vc;
-           }
-               
-        }
-        
-        
-         return vc;
-    }
-        
-};
-
-
-
-/*
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        
-        int left = 0;
-        vector<int> vc;
-
-        for(int i = 0 ; i < nums.size() -1; i++) {
-            for(int j = i+1; j < nums.size(); j++) {
-                if(nums[i] + nums[j] == target){
-                    vc.push_back(i);
-                    vc.push_back(j);
-                    return vc;
-                }
+        vector<int> ans;
+        for(int i = 0 ; i < nums.size(); i++){
+            if(mp.find(target-nums[i]) != mp.end()){
+                ans.push_back(i);
+                ans.push_back(mp[target-nums[i]]);
+                return ans;
             }
+            mp[nums[i]] = i;
         }
-         return vc;
+        return ans;
     }
-        
 };
-*/
